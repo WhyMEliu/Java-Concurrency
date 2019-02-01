08c34397021c6d76c3d3ee6672d33a0855d65a5c
WhyMEliu <527453497@qq.com>
2019-02-01 10:35:36 +0800

    annotations

---
+++ src/main/java/com/peng/concurrency/annotations/NotRecommend.java
@@ -0,0 +1,17 @@
+package com.peng.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记【推荐】的类或者写法
+ * @author peng
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+	
+	String value() default "";
+}
