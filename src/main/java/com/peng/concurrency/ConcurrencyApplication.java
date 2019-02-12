d791fd088c6e9c7d02a3617da8948055242570ad
WhyMEliu <527453497@qq.com>
2019-02-12 21:34:04 +0800

    Hystrix

--- src/main/java/com/peng/concurrency/ConcurrencyApplication.java
+++ src/main/java/com/peng/concurrency/ConcurrencyApplication.java
@@ -3,11 +3,15 @@ package com.peng.concurrency;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ConcurrencyApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
